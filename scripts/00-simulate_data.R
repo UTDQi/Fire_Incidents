cebd5b5794a1efc3406ea0c33dbece4f0d277597
UTDQi <113342231+UTDQi@users.noreply.github.com>
2024-09-26 23:02:46 -0400

    Step1: Simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,35 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates the 
+# Author: David Qi
+# Date: 26 September 2024
+# Contact: david.qi@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(304)
+
+number_of_incidents <- 100
+#let's assume [Dollar loss] = 10000 * 2^(-[number of Responding Personnel]) + error
+#error ~ N(0,10000)
+# [number of Responding Personnel] ~ geom(p = 0.7)
+
+data <-
+  tibble(
+    number_of_responding_personnel = rgeom(x = number_of_incidents, p = 0.7)
+    ,
+    incident_id = c(1:number_of_incidents),
+    dollar_loss = 10000* 2^number_of_responding_personnel + rnorm(number_of_incidents,0,100)
+  )
+
+
+#### Write_csv
+write_csv(data, file = "data/raw_data/simulated.csv")
 
 
 
