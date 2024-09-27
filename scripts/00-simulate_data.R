a96148da75b789afa34ed70465df4d08350124bc
UTDQi <113342231+UTDQi@users.noreply.github.com>
2024-09-27 15:06:42 -0400

    Step4: Clean unused files, add title

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Simulates the 
+# Purpose: Simulates the dollar loss vs. number of responding personnel data
 # Author: David Qi
 # Date: 26 September 2024
 # Contact: david.qi@mail.utoronto.ca
@@ -10,6 +10,7 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(sigmoid)
 
 #### Simulate data ####
 set.seed(304)
@@ -17,14 +18,14 @@ set.seed(304)
 number_of_incidents <- 100
 #let's assume [Dollar loss] = 10000 * 2^(-[number of Responding Personnel]) + error
 #error ~ N(0,10000)
-# [number of Responding Personnel] ~ geom(p = 0.7)
+# [number of Responding Personnel] ~ geom(p = 0.2)
 
 data <-
   tibble(
-    number_of_responding_personnel = rgeom(x = number_of_incidents, p = 0.7)
+    number_of_responding_personnel = rgeom(number_of_incidents, prob = 0.2)
     ,
     incident_id = c(1:number_of_incidents),
-    dollar_loss = 10000* 2^number_of_responding_personnel + rnorm(number_of_incidents,0,100)
+    dollar_loss = relu(10000* 2^(-number_of_responding_personnel) + rnorm(number_of_incidents,0,100))
   )
 
 
